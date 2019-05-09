9f68de06b579747f207a0ab7c1842321ef2b6582
sschoenith <sschoenith@gmail.com>
2019-05-09 16:42:55 -0700

    removing token and adding commented version of token in env variable

--- slack_status.py
+++ slack_status.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python3
+#run this program like so:
+#SLACK_BOT_TOKEN="slack_token" python3 slack_status.py
+
 
 #working remote status update example
-#curl -X POST https://slack.com/api/users.profile.set\?profile\=%7B%0A%20%20%20%20%22status_text%22%3A%20%22working%20remotely%22%2C%0A%20%20%20%20%22status_emoji%22%3A%20%22%3Ahouse_with_garden%3A%22%0A%7D\&token\=xoxp-19620222548-19620222564-611406198307-d7f963d3979e20f29794c0077061dcd0
+#curl -X POST https://slack.com/api/users.profile.set\?profile\=%7B%0A%20%20%20%20%22status_text%22%3A%20%22working%20remotely%22%2C%0A%20%20%20%20%22status_emoji%22%3A%20%22%3Ahouse_with_garden%3A%22%0A%7D\&token\=xoxp-
 
 #working from office status update example
-#curl -X POST https://slack.com/api/users.profile.set\?profile\=%7B%0A%20%20%20%20%22status_text%22%3A%20%22San%20Diego%22%2C%0A%20%20%20%20%22status_emoji%22%3A%20%22%3Apost_office%3A%22%0A%7D\&token\=xoxp-19620222548-19620222564-611406198307-d7f963d3979e20f29794c0077061dcd0
+#curl -X POST https://slack.com/api/users.profile.set\?profile\=%7B%0A%20%20%20%20%22status_text%22%3A%20%22San%20Diego%22%2C%0A%20%20%20%20%22status_emoji%22%3A%20%22%3Apost_office%3A%22%0A%7D\&token\=xoxp-
 
 #example api post request for status update below:
 #POST /api/users.profile.set
 #Host: slack.com
-#Authorization: Bearer xoxp-19620222548-19620222564-611406198307-d7f963d3979e20f29794c0077061dcd0
+#Authorization: Bearer
 #Content-type: application/json; charset=utf-8
 #{
 #    "profile": {
@@ -26,7 +29,8 @@ import requests
 wifi_path = '/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport'
 #wifi_path_cmd = '/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I | awk "/ SSID/ {print substr($0, index($0, $2))}"'
 #remove token from git
-slack_token = 'xoxp-19620222548-19620222564-611406198307-d7f963d3979e20f29794c0077061dcd0'
+slack_token =
+#slack_token = os.environ["SLACK_BOT_TOKEN"]
 slack_headers = {'Authorization': 'Bearer '+ slack_token, 'Content-Type': 'application/json'}
 
 office_ssid = b'NOW-Corporate'
@@ -61,8 +65,6 @@ def wfh_status_update():
     #On your response object you can call: .raise_for_status(). Throw an exception if something wetn wrong, otherwise keep going
     #wfh_status.raise_for_status()
 
-
-
 def update_slack_status():
     try:
         process = subprocess.Popen([wifi_path, '-I'], stdout=subprocess.PIPE)
@@ -74,13 +76,9 @@ def update_slack_status():
         else:
             wfh_status_update()
             print ('WFH')
-    except Exception as e:
-        print('Something failed: ' + e)
-
-
-#update_slack_status()
+    except:
+        print('Something failed')
 
 # add if name = main statement
 if __name__ == '__main__':
     update_slack_status()
-    print('Status update successful')
